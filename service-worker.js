e1e554940ddb246a2d86ec86f0a750660a4acf6d
kroma <kromalee2020@gmail.com>
2025-09-30 15:53:35 +0800

    fix: 移除快速粘贴按钮的多余图标属性

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'coc-timer-v250930154725';
-const STATIC_CACHE = 'coc-timer-static-v250930154725';
-const DYNAMIC_CACHE = 'coc-timer-dynamic-v250930154725';
+const CACHE_NAME = 'coc-timer-v250930155335';
+const STATIC_CACHE = 'coc-timer-static-v250930155335';
+const DYNAMIC_CACHE = 'coc-timer-dynamic-v250930155335';
 
 // 静态资源缓存列表
 const STATIC_ASSETS = [
